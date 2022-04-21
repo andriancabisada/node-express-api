1d4ee98a2de5ed38cfb3fe356908b98372b1a96f
andriancabisada <82685346+andriancabisada@users.noreply.github.com>
2022-04-21 14:50:42 +0800

    update index.js

--- index.js
+++ index.js
@@ -1,93 +1,90 @@
- const express = require('express');
- const bodyParser = require('body-parser');
- const path = require('path');
- const usersRoutes = require('./routes/users.js'); 
- const db = require('./config/database.js');
- const xss = require('xss-clean');
- const mongoSanitize = require('express-mongo-sanitize');
- const helmet = require('helmet');
- const cors = require('cors');
- const passport = require('passport');
- const { errorHandler, notFound } = require('./middleware/error.js');
- const https = require('https');
- const fs = require('fs');
- //Error Middleware
+const express = require("express");
+const bodyParser = require("body-parser");
+const path = require("path");
+const usersRoutes = require("./routes/users.js");
+const db = require("./config/database.js");
+const xss = require("xss-clean");
+const mongoSanitize = require("express-mongo-sanitize");
+const helmet = require("helmet");
+const cors = require("cors");
+const passport = require("passport");
+const { errorHandler, notFound } = require("./middleware/error.js");
+const https = require("https");
+const fs = require("fs");
+//Error Middleware
 //  const { errorHandler, notFound } = require('./middleware/error');
 const startServer = () => {
   const app = express();
 
- const http = require('http');
- const server = http.createServer(app);
- const socket = require('./socket')
- const { Server } = require("socket.io");
- const io = new Server(server, {
-  cors: {
-    origin: "*",
-    methods: ["GET", "POST"]
-  },
- });
+  const http = require("http");
+  const server = http.createServer(app);
+  const socket = require("./socket");
+  const { Server } = require("socket.io");
+  const io = new Server(server, {
+    cors: {
+      origin: "*",
+      methods: ["GET", "POST"],
+    },
+  });
 
-///MIDDLEWARE
-app.use(cors());
-app.use(express.json());
-app.use(passport.initialize());
+  ///MIDDLEWARE
+  app.use(cors());
+  app.use(express.json());
+  app.use(passport.initialize());
 
-//bring in the strategy
-require('./passport_config/passport')(passport);
+  //bring in the strategy
+  require("./passport_config/passport")(passport);
 
-//Additional Routes Security
-app.use(mongoSanitize());
-//app.use(helmet());
-app.use(
-  helmet({
-    contentSecurityPolicy: false,
-  })
-);
-app.use(xss());
+  //Additional Routes Security
+  app.use(mongoSanitize());
+  //app.use(helmet());
+  app.use(
+    helmet({
+      contentSecurityPolicy: false,
+    })
+  );
+  app.use(xss());
 
+  //connect to db
+  db.authenticate()
+    .then(() => console.log("Database connected"))
+    .catch((err) => console.log("Error:" + err));
 
-//connect to db
-db.authenticate()
-    .then(() => console.log('Database connected'))
-    .catch(err => console.log('Error:' + err))
-    
+  //routes
+  app.use("/users", usersRoutes);
 
+  app.get("/", (req, res, next) => {
+    res.send("Hello SSL Homepage");
+  });
 
-//routes
-app.use('/users', usersRoutes);
+  app.use(bodyParser.urlencoded({ extended: false }));
+  app.use(bodyParser.json(), cors());
+  app.options("*", cors());
 
-app.get('/', (req, res, next) =>  {
-  res.send('Hello SSL Homepage')
-});
+  //Error Middleware
+  app.use(notFound);
+  app.use(errorHandler);
 
+  //starting io server
+  socket(io);
 
+  // const PORT = process.env.PORT || 5000;
+  //   app.listen(PORT, () =>   console.log(`Server running on port: http://localhost:${PORT}`));
 
-app.use(bodyParser.urlencoded({extended: false}));
-app.use(bodyParser.json(), cors());
-app.options('*', cors());
+  // app.get('/', (req, res, next) => {
+  //   res.send("Welcome to the home page. API is up and running");
+  // });
 
-//Error Middleware
-app.use(notFound)
-app.use(errorHandler)
+  const sslServer = https.createServer(
+    {
+      key: fs.readFileSync(path.join(__dirname, "cert", "key.pem")),
+      cert: fs.readFileSync(path.join(__dirname, "cert", "cert.pem")),
+    },
+    app
+  );
 
-
-//starting io server
-socket(io)
-
-// const PORT = process.env.PORT || 5000;
-//   app.listen(PORT, () =>   console.log(`Server running on port: http://localhost:${PORT}`));
-
-// app.get('/', (req, res, next) => {
-//   res.send("Welcome to the home page. API is up and running");
-// });
-
-const sslServer = https.createServer({
-  key: fs.readFileSync(path.join(__dirname, 'cert', 'key.pem')),
-  cert: fs.readFileSync(path.join(__dirname, 'cert', 'cert.pem'))
-}, app)
-
-const port = process.env.PORT || 5000;
-sslServer.listen(port, () => console.log(`Server running on port ${port}`));
+  const port = process.env.PORT || 5000;
+  sslServer.listen(port, () => console.log(`Server running on port ${port}`));
 };
 
 startServer();
